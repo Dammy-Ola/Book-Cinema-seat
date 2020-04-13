6cb5d8373d4c864766417adbc08c67bc85d2564c
Dammy-Ola <dammyolapaix@gmail.com>
2020-04-13 02:20:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(occupied)');
+const count = document.querySelector('#count');
+const total = document.querySelector('#total');
+const movieSelect = document.querySelector('#movie');
+
+let ticketPrice = parseInt(movieSelect.value);
+
+// Update total and count
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+};
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = parseInt(e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
